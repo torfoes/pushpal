e952208d581193300925fa8e58a9b487a0b55919
karlos <karloszuru@gmail.com>
2024-09-12 17:51:49 -0500

    Basic table for displaying users

--- frontend/src/app/users/page.tsx
+++ frontend/src/app/users/page.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
+import {columns} from "@/app/users/columns";
+import {DataTable} from "@/app/users/data-table";
 
-const Page = async () => {
+type User = {
+    id: string;
+    name: string;
+    email: string;
+    uin: string;
+    status: "active" | "inactive";
+};
+
+async function getData(): Promise<User[]> {
     const res = await fetch('http://localhost:3000/users', {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
-        cache: 'no-store'
+        cache: 'no-store',
     });
 
-    const users = await res.json();
+    if (!res.ok) {
+        throw new Error('Failed to fetch users');
+    }
+
+    return res.json();
+}
+
+const Page = async () => {
+    const users = await getData()
+    // console.log(users);
 
     return (
         <div>
             <h1>Users List</h1>
             <ul>
-                {users.map((user) => (
-                    <li key={user.id}>{user.email}</li>
-                ))}
+
+                <DataTable columns={columns} data={users}/>
             </ul>
         </div>
     );
