6c7aa877a0ade5cb183aa39d287569fa663da066
karlos <karloszuru@gmail.com>
2024-09-12 18:01:38 -0500

    rails generate scaffold Attendance user:uuid event:uuid time:datetime status:integer

--- backend/app/models/attendance.rb
+++ backend/app/models/attendance.rb
@@ -1,2 +1,8 @@
 class Attendance < ApplicationRecord
+  belongs_to :user
+  belongs_to :event
+
+  enum status: { invited: 0, rsvped: 1, checked_in: 2, cancelled: 3 }
+
+  validates :status, presence: true
 end
