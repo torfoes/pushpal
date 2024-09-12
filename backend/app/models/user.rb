e4435c3a929e05e2f8afdc365998fcb4f2b822df
karlos <karloszuru@gmail.com>
2024-09-12 14:26:41 -0500

    enum role: { attendee: 0, organizer: 1, admin: 2 }

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { attendee: 0, organizer: 1, admin: 2 }
+
 end
