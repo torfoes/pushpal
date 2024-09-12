efb18bb6b134f2debb48a9baf5d97983acd00f8b
karlos <karloszuru@gmail.com>
2024-09-12 16:37:45 -0500

    rails generate migration AddUinToUsers uin:string

---
+++ backend/db/migrate/20240912213730_add_uin_to_users.rb
@@ -0,0 +1,5 @@
+class AddUinToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :uin, :string
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_12_194743) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_12_213730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_12_194743) do
     t.string "name"
     t.string "phone_number"
     t.integer "role"
+    t.string "uin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
