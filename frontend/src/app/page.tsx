6ea80225d0ae5c27267c401f4f551d384f0485d5
karlos <karloszuru@gmail.com>
2024-09-13 01:19:04 -0500

    link to subscribe page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Link from "next/link";
 
 const Page = () => {
     return (
         <div>
             hello world!
+
+            <Link href={"./subscribe"}>
+                subscribe page
+            </Link>
         </div>
     );
 };
