cfcedc397a5d15cdc7b75f7eae8874cfcc971ab9
karlos <karloszuru@gmail.com>
2024-09-12 21:47:12 -0500

    init validation

--- backend/app/models/membership.rb
+++ backend/app/models/membership.rb
@@ -1,4 +1,8 @@
 class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :organization
+
+  enum role: { member: 0, admin: 1 }
+
+  validates :role, presence: true
 end
--- backend/app/models/organization.rb
+++ backend/app/models/organization.rb
@@ -1,2 +1,11 @@
 class Organization < ApplicationRecord
+  has_many :memberships, dependent: :destroy
+  has_many :users, through: :memberships
+  has_many :events, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+  has_many :dues, dependent: :destroy
+  has_many :invitations, dependent: :destroy
+
+  validates :name, presence: true
+
 end
