d42068da2c3d3779b90fa8a898b739806294d072
karlos <karloszuru@gmail.com>
2024-09-13 01:17:11 -0500

    optional user on notification

--- backend/app/models/notification.rb
+++ backend/app/models/notification.rb
@@ -1,8 +1,8 @@
 class Notification < ApplicationRecord
-  belongs_to :user
-  belongs_to :event
+  belongs_to :user, optional: true
   belongs_to :event, optional: true
 
+  validates :message, presence: true
 
   enum send_type: { email: 0, sms: 1, push: 2 }
   enum message_type: { rsvp_request: 0, reminder: 1, ticket_delivery: 2 }
