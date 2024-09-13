c6b37100beebe0e4a126bfa073f6aea9e623577d
karlos <karloszuru@gmail.com>
2024-09-12 22:29:22 -0500

    new resource routes,   resources :organizations and   resources :memberships

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  resources :memberships
   resources :dues
   resources :notifications
   resources :attendances
   resources :events
+  resources :organizations
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :index, :show]
+
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
