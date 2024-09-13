50d426a5f38bcae26d2b3ad6bcfe504c4223a0a2
karlos <karloszuru@gmail.com>
2024-09-12 21:49:31 -0500

    create orgs migration

--- backend/db/migrate/20240913021534_create_organizations.rb
+++ backend/db/migrate/20240913021534_create_organizations.rb
@@ -1,9 +1,8 @@
 class CreateOrganizations < ActiveRecord::Migration[7.0]
   def change
     create_table :organizations, id: :uuid do |t|
-      t.string :name
+      t.string :name, null: false
       t.text :description
-
       t.timestamps
     end
   end
--- backend/db/migrate/20240913022847_create_memberships.rb
+++ backend/db/migrate/20240913022847_create_memberships.rb
@@ -3,9 +3,9 @@ class CreateMemberships < ActiveRecord::Migration[7.0]
     create_table :memberships, id: :uuid do |t|
       t.references :user, null: false, foreign_key: true, type: :uuid
       t.references :organization, null: false, foreign_key: true, type: :uuid
-      t.integer :role
-
+      t.integer :role, null: false, default: 0
       t.timestamps
     end
+    add_index :memberships, [:user_id, :organization_id], unique: true
   end
 end
