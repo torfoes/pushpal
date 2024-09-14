a5f773340f2e12f32ad3ca96cb59491cbcbc85f1
karlos <karloszuru@gmail.com>
2024-09-13 22:26:24 -0500

    oopsie - migration fix

---
+++ backend/db/migrate/20240913024439_create_organizations.rb
@@ -0,0 +1,9 @@
+class CreateOrganizations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :organizations, id: :uuid do |t|
+      t.string :name, null: false
+      t.text :description
+      t.timestamps
+    end
+  end
+end
