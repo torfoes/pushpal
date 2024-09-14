0dac3d7eb4c05d88119fdd5dd6f037cbd8d2c010
karlos <karloszuru@gmail.com>
2024-09-13 22:26:29 -0500

    rm gem 'sorcery'

---
+++ backend/db/migrate/20240914030536_remove_sorcery_fields_from_users.rb
@@ -0,0 +1,6 @@
+class RemoveSorceryFieldsFromUsers < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :users, :crypted_password, :string
+    remove_column :users, :salt, :string
+  end
+end
