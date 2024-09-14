e358efb675f295272acfb22f8e1dfc56054e07d3
karlos <karloszuru@gmail.com>
2024-09-14 12:41:21 -0500

    Function to derive encryption key using HKDF matching the key derivation process from nextauth

---
+++ backend/scripts/hkdf_test.rb
@@ -0,0 +1,19 @@
+require 'openssl'
+
+# Function to derive encryption key using HKDF
+def derive_encryption_key(hash_algo, key_material, salt, info, length)
+  OpenSSL::KDF.hkdf(key_material,
+                    salt: salt,
+                    info: info,
+                    length: length,
+                    hash: hash_algo)
+end
+
+key_material = "xBvLQXMtI7W1BILHGszkhbNpYgOtcaosq64s7Sn6308="
+salt = "authjs.session-token"
+info = "Auth.js Generated Encryption Key (#{salt})"
+length = 64
+
+derived_key = derive_encryption_key('sha256', key_material, salt, info, length)
+
+puts "Derived Key: #{derived_key.unpack1('H*')}"
