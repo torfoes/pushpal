819442f1b32bfbb2582f0ddd5e27bce14cabb094
karlos <karloszuru@gmail.com>
2024-09-14 15:29:11 -0500

    update user fields add picture and sub

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_14_030536) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_14_202631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -91,6 +91,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_14_030536) do
     t.string "phone_number"
     t.integer "role"
     t.string "uin"
+    t.string "sub"
+    t.string "picture"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
