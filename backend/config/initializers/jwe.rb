f442f0e1b445267b3e8ee3aa9e01a4ff5ac42042
karlos <karloszuru@gmail.com>
2024-09-14 15:30:08 -0500

    jwe derived key initialization init

---
+++ backend/config/initializers/jwe.rb
@@ -0,0 +1,34 @@
+require 'jose'
+require 'json'
+require 'base64'
+require 'openssl'
+
+
+def derive_encryption_key(hash_algo, key_material, salt, info, length)
+  OpenSSL::KDF.hkdf(key_material,
+                    salt: salt,
+                    info: info,
+                    length: length,
+                    hash: hash_algo)
+end
+
+
+AUTH_SECRET = ENV["AUTH_SECRET"]
+
+Rails.logger.info("Decoded JWT: #{AUTH_SECRET}")
+
+unless AUTH_SECRET.present?
+  raise "AUTH_SECRET environment variable is missing!"
+end
+
+
+salt = "authjs.session-token"
+info = "Auth.js Generated Encryption Key (#{salt})"
+length = 64
+
+derived_key = derive_encryption_key('sha256', AUTH_SECRET, salt, info, length)
+
+
+JWK_OCT512 = JOSE::JWK.from_oct(derived_key)
+
+# puts "Derived Key (Hex): #{DERIVED_KEY_HEX}" if Rails.env.development?
