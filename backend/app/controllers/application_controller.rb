39542c0628530ab8e49e5334a23305fda7e8b62a
karlos <karloszuru@gmail.com>
2024-09-14 15:49:24 -0500

    authentication required for ALL routes.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # for csrf
   skip_before_action :verify_authenticity_token
 
+  include Authentication
+  before_action :authenticate_request
 end
