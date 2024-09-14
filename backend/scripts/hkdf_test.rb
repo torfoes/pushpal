94f1bd636dc6c7ae9af5f3f2a7a9f7a0d290e7a8
karlos <karloszuru@gmail.com>
2024-09-14 15:29:53 -0500

    jwt experiments

--- backend/scripts/hkdf_test.rb
+++ backend/scripts/hkdf_test.rb
@@ -9,6 +9,7 @@ def derive_encryption_key(hash_algo, key_material, salt, info, length)
                     hash: hash_algo)
 end
 
+# this key is not in use - this is simply to demonstrate the process.
 key_material = "xBvLQXMtI7W1BILHGszkhbNpYgOtcaosq64s7Sn6308="
 salt = "authjs.session-token"
 info = "Auth.js Generated Encryption Key (#{salt})"
