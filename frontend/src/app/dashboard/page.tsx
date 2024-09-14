b6ba8c9a60a9b6dde2ee1abe33e6907d4fba1265
karlos <karloszuru@gmail.com>
2024-09-13 21:10:09 -0500

    init dashboard page

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            this is the dashboard page. this will contain the organizations that the client is managing.
+        </div>
+    );
+};
+
+export default Page;
