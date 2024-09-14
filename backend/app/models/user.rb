528db637577b282ef82cb2065e8f33c55ce2f05f
karlos <karloszuru@gmail.com>
2024-09-14 15:30:48 -0500

    removed validators for removed fields

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,18 +1,12 @@
 class User < ApplicationRecord
   self.primary_key = 'id'
 
-  authenticates_with_sorcery!
-
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
 
   has_many :push_subscriptions, dependent: :destroy
   has_many :notifications, dependent: :nullify
 
-  # Add validations
-  # validates :uin, presence: false, uniqueness: true, length: { is: 9 }, numericality: { only_integer: true }
-  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :email, uniqueness: true
+  # validates :picture, url: true, allow_nil: true
 end
