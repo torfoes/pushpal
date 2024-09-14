ec6a33c47854b292ab87a16cb7b1acbf70130fb2
karlos <karloszuru@gmail.com>
2024-09-14 15:32:30 -0500

    init install page

---
+++ frontend/src/app/install/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            A tutorial page with animated gifs showing the install process for different platforms
+        </div>
+    );
+};
+
+export default Page;
