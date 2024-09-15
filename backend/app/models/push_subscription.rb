7436f2fe910e363695e8ad019e06ba15d583fa4b
karlos <karloszuru@gmail.com>
2024-09-15 14:45:31 -0500

    PushSubscription belongs_to :user, optional: true -- this gonna make things a lot cleaner

--- backend/app/models/push_subscription.rb
+++ backend/app/models/push_subscription.rb
@@ -1,5 +1,5 @@
 class PushSubscription < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user, optional: false
 
   validates :endpoint, presence: true, uniqueness: true
   validates :p256dh_key, :auth_key, presence: true
