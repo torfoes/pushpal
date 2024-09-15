46b1b4c7173a447e60e6a308875b8bb2d3f4ca04
karlos <karloszuru@gmail.com>
2024-09-15 17:39:39 -0500

    config.api_only = true

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -9,6 +9,8 @@ module Notifyme
   class Application < Rails::Application
     config.load_defaults 7.0
     config.active_record.primary_key = :uuid
+    config.api_only = true
+
 
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
