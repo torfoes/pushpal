8d9ce814189f33dc17f9c2246415ebedb2c78491
karlos <karloszuru@gmail.com>
2024-09-17 15:38:42 -0500

    added Toaster component

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import {auth} from "@/lib/auth";
+import {Toaster} from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -22,8 +23,9 @@ export default async function RootLayout({
 
           <body className={inter.className}>
           <Header session={session}/>
-
           {children}
+          <Toaster />
+
           </body>
         </html>
   );
