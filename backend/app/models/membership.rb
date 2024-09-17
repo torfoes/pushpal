172eefe754b59076ce729d91e5b987d008003e40
karlos <karloszuru@gmail.com>
2024-09-17 00:08:29 -0500

    changed roles to be   enum role: {             member: 0,             creator: 1,             manager: 2           }

--- backend/app/models/membership.rb
+++ backend/app/models/membership.rb
@@ -2,7 +2,11 @@ class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :organization
 
-  enum role: { member: 0, creator: 1, manage: 2 }
+  enum role: {
+    member: 0,
+    creator: 1,
+    manager: 2
+  }
 
   validates :user_id, :organization_id, :role, presence: true
 end
