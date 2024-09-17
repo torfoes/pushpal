e06aeeec4b0f9ea5cca0e690cba31dd421030b89
karlos <karloszuru@gmail.com>
2024-09-17 00:07:59 -0500

    rm boilerplate comments

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
-             expose: ['Authorization'], # Expose specific headers if needed
+             expose: ['Authorization'],
              max_age: 600
   end
 end
