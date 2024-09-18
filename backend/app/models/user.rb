1ab11495aac7106622a8d70c47cc0bab71602836
karlos <karloszuru@gmail.com>
2024-09-18 00:20:26 -0500

    central member model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_many :memberships, dependent: :destroy
-  has_many :organizations, through: :memberships
-
   has_many :push_subscriptions, dependent: :destroy
-  has_many :notifications, dependent: :nullify
+
+  has_many :organizations, through: :memberships
 
   validates :email, uniqueness: true
 end
