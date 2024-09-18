530a91eb14165fca8da2c759abe31b0dbc68c551
karlos <karloszuru@gmail.com>
2024-09-18 00:20:58 -0500

    removed login routes from old auth framework (sorcery)

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -15,10 +15,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :index, :show]
 
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
