b18f21d184e6aca2d4f3c9b95d9703dae59b3bc7
karlos <karloszuru@gmail.com>
2024-09-19 12:58:37 -0500

    routes for sending push notifications

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,13 +5,18 @@ Rails.application.routes.draw do
   resources :attendances
   resources :events
   resources :organizations
-  resources :push_subscriptions, path: 'push-subscriptions', only: [:create, :destroy, :index, :show]
+  resources :push_subscriptions, path: 'push-subscriptions', only: [:create, :destroy, :index, :show] do
+    member do
+      post 'send_notification'
+    end
+  end
 
   resources :organizations, only: [] do
     member do
       post 'send_push_notifications'
     end
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :index, :show]
 
