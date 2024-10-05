5fa73d418c702954a7aa9dba4f19b9e79a825091
karlos <karloszuru@gmail.com>
2024-10-05 14:45:45 -0500

    left a note for people whenever they stumble across this page

--- frontend/src/app/dashboard/[organization_id]/[event_id]/page.tsx
+++ frontend/src/app/dashboard/[organization_id]/[event_id]/page.tsx
@@ -1,8 +1,11 @@
 export default async function Page({ params }: { params: { event_id: string } }) {
 
     return(
-        <>
-            {params.event_id}
-        </>
+        <div className={''}>
+            this is in the [event_id] route btw - it will match anything that isnt a route already
+            <div className={'text-2xl'}>
+             {params.event_id}
+            </div>
+        </div>
     )
 }
