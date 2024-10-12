2eecf68dcee5f2b70080d05236d192a3b3330def
karlos <karloszuru@gmail.com>
2024-10-11 22:23:33 -0500

    <h2 className="text-2xl font-semibold">Events</h2>

--- frontend/src/app/dashboard/[organization_id]/events/page.tsx
+++ frontend/src/app/dashboard/[organization_id]/events/page.tsx
@@ -1,25 +1,20 @@
-import { getSessionTokenOrRedirect } from "@/app/utils";
 import CreateEventDialog from "@/app/dashboard/[organization_id]/events/CreateEventDialog";
-import { Event } from "@/types";
 import EventList from "@/app/dashboard/[organization_id]/events/EventList";
 import { getOrganizationEvents } from './actions'
 
-
 export default async function Page({ params }: { params: { organization_id: string } }) {
     const { organization_id } = params;
-    const events = await getOrganizationEvents(organization_id);  // Fetch events for the organization
+    const events = await getOrganizationEvents(organization_id);
 
     return (
-        <div className="container mx-auto p-4">
+        <div className="container">
             <div className="flex justify-between items-center mb-6">
-                <h1 className="text-3xl font-bold">Organization Events</h1>
+                <h2 className="text-2xl font-semibold">Events</h2>
                 <div className="flex items-center space-x-4">
                     <CreateEventDialog organization_id={organization_id} />
                 </div>
             </div>
-            <div>
                 <EventList events={events} />
-            </div>
         </div>
     );
 }
