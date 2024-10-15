aaf488a6038604088d1b61ad66542b546c8fb6f2
karlos <karloszuru@gmail.com>
2024-10-15 15:24:09 -0500

    upcoming events and recent notification fetching

--- frontend/src/app/dashboard/[organization_id]/overview/page.tsx
+++ frontend/src/app/dashboard/[organization_id]/overview/page.tsx
@@ -1,11 +1,74 @@
 import React from 'react';
+import {MemberInfo} from "@/types";
+import {getSessionTokenOrRedirect} from "@/app/utils";
 
-const Page = () => {
+async function fetchUpcomingEvents(organization_id: string): Promise<MemberInfo> {
+    const sessionToken = await getSessionTokenOrRedirect();
+
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_RAILS_SERVER_URL}organizations/${organization_id}/events/upcoming`,
+        {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${sessionToken}`,
+                'Content-Type': 'application/json',
+            },
+            cache: 'no-cache',
+        }
+    );
+
+    if (res.status === 404) {
+        throw new Error('Not found');
+    }
+
+    if (!res.ok) {
+        const errorDetails = await res.json();
+        console.error('Failed to fetch upcoming events', errorDetails);
+        throw new Error(`Failed to fetch upcoming events: ${res.status}`);
+    }
+
+    return res.json();
+}
+
+async function fetchRecentNotifications(organization_id: string): Promise<MemberInfo> {
+    const sessionToken = await getSessionTokenOrRedirect();
+
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_RAILS_SERVER_URL}organizations/${organization_id}/notifications/recent`,
+        {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${sessionToken}`,
+                'Content-Type': 'application/json',
+            },
+            cache: 'no-cache',
+        }
+    );
+
+    if (res.status === 404) {
+        throw new Error('Not found');
+    }
+
+    if (!res.ok) {
+        const errorDetails = await res.json();
+        console.error('Failed to fetch recent notifications', errorDetails);
+        throw new Error(`Failed to fetch  recent notifications: ${res.status}`);
+    }
+
+    return res.json();
+}
+
+export default async function OverviewPage({ params }: { params: { organization_id: string } }) {
+    const upcomingEvents = await fetchUpcomingEvents(params.organization_id);
+    const recentNotifications = await fetchRecentNotifications(params.organization_id);
+
+    console.log(upcomingEvents);
+    console.log(recentNotifications);
+
+
     return (
         <div>
             overview
         </div>
     );
 };
-
-export default Page;
