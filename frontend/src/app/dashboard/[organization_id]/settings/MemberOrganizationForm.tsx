4614ddf97d4344f250d1eeb6b5fa5e2cb0abbd0b
karlos <karloszuru@gmail.com>
2024-10-24 20:37:32 -0500

    member organization form

--- frontend/src/app/dashboard/[organization_id]/settings/MemberOrganizationForm.tsx
+++ frontend/src/app/dashboard/[organization_id]/settings/MemberOrganizationForm.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Organization} from "@/types";
 
-const MemberOrganizationForm = () => {
+interface OrganizationFormProps {
+    organization: Organization;
+}
+export default function MemberOrganizationForm({ organization }: OrganizationFormProps) {
     return (
         <div>
             do it here
         </div>
     );
 };
-
-export default MemberOrganizationForm;
