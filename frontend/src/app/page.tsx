db849bb274fcd7d9464634e0e2205e40328e08e8
karlos <karloszuru@gmail.com>
2024-11-07 00:31:02 -0600

    homepage is better centered

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,19 +5,19 @@ import Image from "next/image";
 const Page = () => {
     return (
         <main className="flex-1">
-            <section className="w-full min-h-screen flex justify-center py-12 md:py-24 lg:py-32 xl:py-48">
+            <section className="w-full h-screen flex items-center justify-center">
                 <div className="container px-4 md:px-6">
-                    <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+                    <div className="grid gap-6 md:grid-cols-2 lg:gap-12">
                         <div className="flex flex-col justify-center space-y-4">
                             <div className="space-y-2">
-                                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
                                     Keep Everyone in the Loop with PushPal
                                 </h1>
                                 <p className="max-w-[600px] text-muted-foreground md:text-xl">
                                     Manage attendance and check-ins with PushPal. Keep everyone in the loop with real-time push notifications and ensure your events run smoothly from start to finish.
                                 </p>
                             </div>
-                            <div className="flex flex-col gap-2 min-[400px]:flex-row">
+                            <div className="flex flex-col gap-2 sm:flex-row">
                                 <Link
                                     className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                                     href="./login"
@@ -36,13 +36,12 @@ const Page = () => {
                         <div className="flex justify-center">
                             <Image
                                 src="/iphone_push.png"
-                                alt="Push Pal Demo"
+                                alt="PushPal Demo"
                                 className="rounded-lg shadow-lg w-full h-auto max-w-sm"
                                 width={400}
                                 height={400}
                             />
                         </div>
-
                     </div>
                 </div>
             </section>
